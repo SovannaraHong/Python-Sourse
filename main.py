3256dfe6b3a84795fa98910feca66c45e8c0259d
Hong Sovannara <rapewpew372@gmail.com>
2025-09-20 20:58:39 +0700

    update string

--- main.py
+++ main.py
@@ -18,3 +18,17 @@ num_of_students=10
 print(f"My age is {age} years olds")
 print(f"You are buying {quantity} items")
 print(f"Your class is {num_of_students} students in class")
+
+
+
+#float
+
+price =10.224
+gpa=3.0
+print(f"your item is price of : {price}")
+print(f"my gpa is : {gpa}")
+
+
+
+isStudent =True
+print(isStudent)
