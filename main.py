01af4558bf30d9924fc6b8c6635eb99554c9350c
Hong Sovannara <rapewpew372@gmail.com>
2025-09-20 19:46:50 +0700

    update python

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("i like pizza")
+
+# variable
+
+
+#string =======================
+
+firt_name="Hong sovannara"
+food="pizza"
+
+print(f"first name: {firt_name}")
+print(f"I love food: {food}")
+
+#intiger
+
+age =23
+quantity=3
+num_of_students=10
+print(f"My age is {age} years olds")
+print(f"You are buying {quantity} items")
+print(f"Your class is {num_of_students} students in class")
