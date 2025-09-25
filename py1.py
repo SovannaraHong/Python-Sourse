922f6ea4593feacc115eaf62519c163fd2505686
Hong Sovannara <rapewpew372@gmail.com>
2025-09-25 21:44:42 +0700

    p1

---
+++ py1.py
@@ -0,0 +1,12 @@
+name ="akira"
+age =20
+is_Student =True
+is_Single =False
+drink ="alcohol"
+
+print(f"my name is {name}")
+print(f"i am {age} years old")
+if(is_Student):
+    print(f"you cant drink {drink}")
+else:
+    print(f"you can drink {drink}")
