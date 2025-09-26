5ee5b9e73aa5c6354483e08d60d28f479657334c
Hong Sovannara <rapewpew372@gmail.com>
2025-09-26 19:31:59 +0700

    p2 exercise

---
+++ py3.py
@@ -0,0 +1,14 @@
+#math function
+
+
+x=3.14
+y=2
+z=5
+
+# result=round(x) the result it show 3
+#abs=absolute value
+# result=abs(y)
+# result =pow(4,3) it show 64 it 4 svaykun 3
+# result =max (x,y,z) show big
+# result =min(x,y,z) show small
+# print(result)
---
+++ py4.py
@@ -0,0 +1,7 @@
+import math
+
+x =4
+# print(math.ceil(x)) convert up
+# print(math.floor(x)) convert down
+# print(round(x)) check big up small down
+# print(math.sqrt(x)) it jek  like 4/2 =2.0
