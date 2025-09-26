ba2ed3ada358b58981810e63adca6c92f6861bc0
Hong Sovannara <rapewpew372@gmail.com>
2025-09-26 15:53:48 +0700

    p2 exercise

---
+++ Exercise/MadlistGame.py
@@ -0,0 +1,7 @@
+
+word1 =input("Enter the word (description): ")
+abjective1=input("Enter the word (description): ")
+abjective2=input("Enter the word (description): ")
+print(f"The oldest classical British and Latin writings had little or no space between words and could be written in {word1}")
+print(f"Over time, text direction (left to right) became standardized. {abjective1}")
+print(f"and {abjective2}")
