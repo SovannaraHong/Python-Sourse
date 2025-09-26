a6d80d07429db6b12bb018859d4be81ea47ebf14
Hong Sovannara <rapewpew372@gmail.com>
2025-09-26 15:23:15 +0700

    p2 exercise

---
+++ Exercise/ShoppingCart.py
@@ -0,0 +1,6 @@
+food =str(input("Enter the food you would like to buy: "))
+price=float(input("Enter the price you would like to buy: "))
+qty=int(input("Enter the quantity you would like to buy: "))
+
+area =price*qty
+print(f"Your total area is : {area}")
