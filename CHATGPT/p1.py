524cdc509316c4eb76632fa5ae016d9942d97e73
Hong Sovannara <rapewpew372@gmail.com>
2025-09-27 20:45:10 +0700

    p6

---
+++ CHATGPT/p1.py
@@ -0,0 +1,14 @@
+num =int(input("enter a number: " ))
+result = "EVEN" if num %2 ==0 else "ODD"
+print(result)
+
+
+mark = int(input("enter your mark: "))
+resultMark ="PASS" if mark>=50 else "FAIL"
+print(resultMark)
+
+checkNum = int(input("enter a number: "))
+resultNum ="POSITIVE" if checkNum>0 else "NEGATIVE"
+print(resultNum)
+
+
---
+++ p7.py
@@ -0,0 +1,18 @@
+#method in python have len find rfind
+
+name =input("enter your name:")
+phone_number=input("enter your phone number:")
+# result =name.find("g")check the index
+# result =len(name)
+# result =name.rfind("n") revers check letter
+# result =name.capitalize() input hong output Hong
+# result =name.upper()
+# result =name.lower()
+# result =name.isdigit() return true or false it true only number
+# result =name.isalpha() return false when have space in string and string with number like nara123
+# print(result)
+
+
+# result =phone_number.count("+") count item
+print(result)
+
