4525d75e1edd0c2e095c8d1518d6334ac2f48ec7
Hong Sovannara <rapewpew372@gmail.com>
2025-09-28 20:19:32 +0700

    p8

--- p8.py
+++ p8.py
@@ -1,9 +1,11 @@
 name =input("enter your name:")
 if len(name)>12:
     print("name too long than 12 characters")
-elif name.find(" ")!= -1:
-    print("name contains only single characters")
+elif  name.find(" ")!= -1: #input _nara
+    print(f"your {name} cant use with contain space")
+elif len(name)==0:
+    print("name cant be an empty string")
 elif not name.isalpha():
-    print("name no need contains space ")
+    print(f"your {name} cant use with number or space ")
 else:
     print(f"your {name} is correct condition")
