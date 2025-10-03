bad4f581b28e9f01f21cc3e98a44f2abc19d41bf
Hong Sovannara <rapewpew372@gmail.com>
2025-10-03 15:53:38 +0700

    exericse countdown

---
+++ Exercise/CountdownNameHappybirdday.py
@@ -0,0 +1,11 @@
+import time
+times=int(input("Enter times: "))
+age =int(input("Enter Your age: "))
+
+for x in range(times):
+    print(x)
+
+    time.sleep(1)
+print(f"Happy Bird Day to you you get age is: {x} years old")
+
+
---
+++ p14.py
@@ -0,0 +1,41 @@
+
+# for x in range(1,3):
+#     for y in range(1,10):
+#         for z in range(1,5):
+#             print(x,y,z, end="")
+# print()
+            #3
+
+
+# row =int(input("enter row: "))
+# col =int(input("enter column: "))
+# symbol =str(input("enter symbol: "))
+# for x in range(row):
+#     for y in range(col):
+#
+#             print(symbol ,end="")
+#     print()
+#
+#
+
+
+#loop data in arr ============================================
+
+
+fruits=["apple","banana","orange","coconut"]
+# print(fruits)
+# for x in fruits:
+#     print(x)
+# print(dir(fruits)) #showing function all
+# print(len(fruits)) # count in arr or count text in string
+# print(help(fruits)) #show detail
+# print("apple" in fruits) # return true false
+# fruits.append("mango")
+# fruits.remove("banana")
+# fruits.insert(2,"mango") #insert by index
+# fruits.sort() #sort a-z and z-a
+# fruits.reverse() #reverse item in arrr not revers by z-a
+
+# fruits.clear() #it clear all items
+# print(fruits.index("banana")) #print output by index
+# print(fruits.count("orange")) #count item in arr
